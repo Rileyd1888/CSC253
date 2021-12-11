0f5eb8b8bd80e87c47fd3f22fc5433ca13cb6d77
Rileyd1888 <52174567+Rileyd1888@users.noreply.github.com>
2021-12-10 21:16:04 -0500

    Add files via upload

---
+++ Reading_Files_RileyDerek/ReadingFilesUI/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿
+namespace ReadingFilesUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.clearButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.showButton = new System.Windows.Forms.Button();
+            this.answerListBox = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(86, 226);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(120, 23);
+            this.clearButton.TabIndex = 0;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(86, 255);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(120, 23);
+            this.exitButton.TabIndex = 1;
+            this.exitButton.Text = "Exit ";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // showButton
+            // 
+            this.showButton.Location = new System.Drawing.Point(86, 197);
+            this.showButton.Name = "showButton";
+            this.showButton.Size = new System.Drawing.Size(120, 23);
+            this.showButton.TabIndex = 2;
+            this.showButton.Text = "Show Results";
+            this.showButton.UseVisualStyleBackColor = true;
+            this.showButton.Click += new System.EventHandler(this.showButton_Click);
+            // 
+            // answerListBox
+            // 
+            this.answerListBox.FormattingEnabled = true;
+            this.answerListBox.Location = new System.Drawing.Point(86, 87);
+            this.answerListBox.Name = "answerListBox";
+            this.answerListBox.Size = new System.Drawing.Size(120, 95);
+            this.answerListBox.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label1.Location = new System.Drawing.Point(42, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(224, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Showing the total of the random file I created :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(311, 290);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.answerListBox);
+            this.Controls.Add(this.showButton);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.clearButton);
+            this.Name = "Form1";
+            this.Text = "Random Number Reader";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Button showButton;
+        private System.Windows.Forms.ListBox answerListBox;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Reading_Files_RileyDerek/ReadingFilesUI/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ReadingFromFileLibrary;
+using System.IO;
+//
+// CSC 253 
+// Derek Terrell Riley 
+// 09/19/21
+// Reading from the file I created of random numbers 
+//
+//
+
+namespace ReadingFilesUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+               //show in answer in listbox
+                answerListBox.Items.Add(ReadingNumber.fileReading().ToString());
+                answerListBox.Items.Add(ReadingNumber.fileCount().ToString());
+                int exceptionReturn = ReadingNumber.fileReading();
+        }
+
+
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            // Clear list box
+            answerListBox.Items.Clear();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // close 
+            this.Close();
+        }
+    }
+}
