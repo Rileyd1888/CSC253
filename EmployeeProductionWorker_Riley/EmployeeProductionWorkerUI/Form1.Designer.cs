a0b7772c4ca6e7efa26d32d604d2135d59392581
Rileyd1888 <52174567+Rileyd1888@users.noreply.github.com>
2021-12-10 21:27:45 -0500

    Add files via upload

---
+++ EmployeeProductionWorker_Riley/EmployeeProductionWorkerUI/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿
+namespace EmployeeProductionWorkerUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.informationBox = new System.Windows.Forms.Label();
+            this.hprTextBox = new System.Windows.Forms.TextBox();
+            this.snTextBox = new System.Windows.Forms.TextBox();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.enTextBox = new System.Windows.Forms.TextBox();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.showButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter Name: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Enter Employee Number: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Enter Shift Number: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(26, 152);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(115, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Enter Hourly Pay Rate:";
+            // 
+            // informationBox
+            // 
+            this.informationBox.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.informationBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.informationBox.Location = new System.Drawing.Point(100, 184);
+            this.informationBox.Name = "informationBox";
+            this.informationBox.Size = new System.Drawing.Size(243, 88);
+            this.informationBox.TabIndex = 4;
+            this.informationBox.Text = "0";
+            this.informationBox.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // hprTextBox
+            // 
+            this.hprTextBox.Location = new System.Drawing.Point(179, 145);
+            this.hprTextBox.Name = "hprTextBox";
+            this.hprTextBox.Size = new System.Drawing.Size(100, 20);
+            this.hprTextBox.TabIndex = 5;
+            // 
+            // snTextBox
+            // 
+            this.snTextBox.Location = new System.Drawing.Point(179, 102);
+            this.snTextBox.Name = "snTextBox";
+            this.snTextBox.Size = new System.Drawing.Size(100, 20);
+            this.snTextBox.TabIndex = 6;
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(179, 19);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.nameTextBox.TabIndex = 7;
+            // 
+            // enTextBox
+            // 
+            this.enTextBox.Location = new System.Drawing.Point(179, 59);
+            this.enTextBox.Name = "enTextBox";
+            this.enTextBox.Size = new System.Drawing.Size(100, 20);
+            this.enTextBox.TabIndex = 8;
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(188, 311);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 9;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // showButton
+            // 
+            this.showButton.Location = new System.Drawing.Point(53, 311);
+            this.showButton.Name = "showButton";
+            this.showButton.Size = new System.Drawing.Size(75, 23);
+            this.showButton.TabIndex = 10;
+            this.showButton.Text = "Show ";
+            this.showButton.UseVisualStyleBackColor = true;
+            this.showButton.Click += new System.EventHandler(this.showButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(317, 311);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 11;
+            this.exitButton.Text = "Exit ";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 392);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.showButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.enTextBox);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.snTextBox);
+            this.Controls.Add(this.hprTextBox);
+            this.Controls.Add(this.informationBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Employee and Production Worker Class";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label informationBox;
+        private System.Windows.Forms.TextBox hprTextBox;
+        private System.Windows.Forms.TextBox snTextBox;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.TextBox enTextBox;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button showButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ EmployeeProductionWorker_Riley/EmployeeProductionWorkerUI/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using EPWorkerLibrary;
+
+//
+//10/24/21  
+//CSC 253
+//Derek Riley 
+//Creating an employee class that has properties and displays the objects properties
+//
+
+namespace EmployeeProductionWorkerUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            // Creating the object
+            ProductionWorker myproductionWorker = new ProductionWorker();
+
+            myproductionWorker.Name = nameTextBox.Text;
+            myproductionWorker.Number = int.Parse(enTextBox.Text.ToString());
+            myproductionWorker.Shiftnumber = int.Parse(snTextBox.Text.ToString());
+            myproductionWorker.HPR = decimal.Parse(hprTextBox.Text.ToString());
+
+            //decision structure 
+            if (myproductionWorker.Shiftnumber == 1)
+            {
+                informationBox.Text = myproductionWorker.Name + " " + myproductionWorker.Number + " " + "Day" + " " + myproductionWorker.HPR.ToString("c");
+            }
+            else
+            {
+                informationBox.Text = myproductionWorker.Name + " " + myproductionWorker.Number + " " + "Night" + " " + myproductionWorker.HPR.ToString("c");
+            }
+           
+      
+            
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            //clearing information
+            informationBox.Text = " ";
+            nameTextBox.Text = " ";
+            enTextBox.Text = " ";
+            snTextBox.Text = " ";
+            hprTextBox.Text = " ";
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            //Close the application
+            this.Close();
+        }
+    }
+}
